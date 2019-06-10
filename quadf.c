1a7a7878577a117aee343777aa85ae6cb74f64be
mattlourenco27 <matt.lourenco27@gmail.com>
2019-06-09 21:14:54 -0400

    Finished calculation of all case solutions
    The discriminant is calculated. Then the solutions are calculated based
    on the value of the discriminant.
    
    if the discriminant is positive it gives both solutions
    
    if the discriminant is zero it gives the sole solution
    
    if the discriminant is negative it give the imaginary solutinos

--- quadf.c
+++ quadf.c
@@ -7,6 +7,7 @@
  */
 
 #include <stdio.h>
+#include <stdbool.h>
 #include <math.h>
 
 //This function displays a message then recieves the respose
@@ -24,10 +25,33 @@ double discriminant(double A, double B, double C) {
 	return pow(B,2) - (4 * A * C);
 }
 
-int main(int argc, char **argv)
-{
+//This function calculates the solution to the quadratic formula with two solutions
+//'pos' dictates if it uses the plus or minus variant of the formula
+double twoSolution(double A, double B, double C, bool pos) {
+	if(pos) {
+		return (-B + sqrt(discriminant(A, B, C))) / (2 * A);
+	} else {
+		return (-B - sqrt(discriminant(A, B, C))) / (2 * A);
+	}
+}
+
+//This function calculates the solution to the quadratic formula with one solution
+double oneSolution(double A, double B) {
+	return -B / (2 * A);
+}
+
+//This function calculates the solution to the quadratic formula with two imaginary solutions
+//The pointer '*img' points to the value of the imaginary component
+double imaginarySolutions(double A, double B, double C, double *img) {
+	*img = sqrt(-discriminant(A, B, C)) / (2 * A);
+	return -B / (2 * A); 
+}
+
+int main(int argc, char **argv) {
 	double A = 0.0, B = 0.0, C = 0.0;
 	double discr = 0.0;
+	
+	unsigned const char PLUS_OR_MINUS= 241;
 	
 	printf("Let the form of the quadratic function be: Ax^2 + Bx + C\n\n");
 	
@@ -35,10 +59,30 @@ int main(int argc, char **argv)
 	B = getRealInput("Input B: ");
 	C = getRealInput("Input C: ");
 	
-	printf("\nA: %.12lf\nB: %.12lf\nC: %.12lf\n", A, B, C);
+	//printf("\nA: %.12lf\nB: %.12lf\nC: %.12lf\n", A, B, C);
 	
 	discr = discriminant(A, B, C);
-	printf("\nThe discriminant is: %.12lf\n", discr);
+	//printf("\nThe discriminant is: %.12lf\n", discr);
+	
+	if (discr > 0) {
+		
+		double posSol = twoSolution(A, B, C, true);
+		double negSol = twoSolution(A, B, C, false);
+		
+		printf("\nSolution(+): %.12lf\nSolution(-): %.12lf\n", posSol, negSol);
+	} else if (discr == 0) {
+		
+		double sol = oneSolution(A, B);
+		
+		printf("\nSingle Solution: %.12lf\n", sol);
+	} else {
+		
+		double imgSol = 0.0;
+		double realSol = imaginarySolutions(A, B, C, &imgSol);
+		
+		printf("\nReal Component: %.12lf\nImaginary Component: %.12lf\n\n%.12lf %c i%.12lf\n",
+			   realSol, imgSol, realSol, PLUS_OR_MINUS, imgSol);
+	}
 	
 	return 0;
 }
