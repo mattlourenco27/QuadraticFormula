b8e3e8d3cf4baaeddc2c1956e070f51c87c01401
mattlourenco27 <matt.lourenco27@gmail.com>
2019-06-23 17:05:34 -0400

    Header file can be imported and quadf() can be executed

--- quadf.h
+++ quadf.h
@@ -3,7 +3,11 @@
  * Created by: Matthew Lourenco
  * This file calculates the solution(s) to the quadratic formula 
  * Ax^2 + Bx + C = 0
- * given Coefficients A, B, and C 
+ * given Coefficients A, B, and C
+ * 
+ * Edited: June 23 2019
+ * Changed file to a header file to be implemented in other programs
+ *
  */
 
 #include <stdio.h>
@@ -11,24 +15,14 @@
 #include <math.h>
 #include <string.h>
 
-//This function displays a message then recieves the respose
-double getRealInput(char *message) {
-	double response = 0.0;
-	
-	printf(message);
-	scanf("%lf", &response);
-	
-	return response;
-}
-
 //This function calculates the discriminant given A, B, C
-double discriminant(double A, double B, double C) {
+static double discriminant(double A, double B, double C) {
 	return pow(B,2) - (4 * A * C);
 }
 
 //This function calculates the solution to the quadratic formula with two solutions
 //'pos' dictates if it uses the plus or minus variant of the formula
-double twoSolution(double A, double B, double C, bool pos) {
+static double twoSolution(double A, double B, double C, bool pos) {
 	if(pos) {
 		return (-B + sqrt(discriminant(A, B, C))) / (2 * A);
 	} else {
@@ -37,75 +31,22 @@ double twoSolution(double A, double B, double C, bool pos) {
 }
 
 //This function calculates the solution to the quadratic formula with one solution
-double oneSolution(double A, double B) {
+static double oneSolution(double A, double B) {
 	return -B / (2 * A);
 }
 
 //This function calculates the solution to the quadratic formula with two imaginary solutions
 //The pointer '*img' points to the value of the imaginary component
-double imaginarySolutions(double A, double B, double C, double *img) {
+static double imaginarySolutions(double A, double B, double C, double *img) {
 	*img = sqrt(-discriminant(A, B, C)) / (2 * A);
 	return -B / (2 * A); 
 }
 
-//This funtion runs the closing procedure
-void close(void) {
-	printf("\nPress ENTER key to Continue\n");
-	getchar();
-	return;
-}
-
-/*
-//This function is a test function that lists out argv and argc for testing
-void listOut(int argc, char **argv) {
-	printf("argc -> %d\n\n", argc);
-
-	for(int i = 0; i < argc; i++) {
-		printf("argv[%d] -> %s\n", i, *argv);
-		argv++;
-	}
-}
-*/
-
-int main(int argc, char **argv) {
-	double A = 0.0, B = 0.0, C = 0.0;
+//This function calculates and prints the quadratic formula
+void quadf(double A, double B, double C) {
 	double discr = 0.0;
 	
 	unsigned const char PLUS_OR_MINUS= 241;
-	
-	//Switch stout to unbuffered mode to fix scan before inputs
-	setvbuf(stdout, 0, _IONBF, 0);
-
-	//Listout the arguments for testing
-	//listOut(argc, argv);
-
-	if (argc > 1) {
-		A = atof(argv[1]);
-
-		if (A == 0) {
-			printf("Error: Function is not quadratic;\n");
-			close();
-			return 0;
-		}
-		B = atof(argv[2]);
-		C = atof(argv[3]);
-
-	} else {
-		printf("Let the form of the quadratic function be: Ax^2 + Bx + C\n\n");
-	
-		A = getRealInput("Input A: ");
-
-		if (A == 0) {
-			printf("Error: Function is not quadratic;\n");
-			close();
-			return 0;
-		}
-
-		B = getRealInput("Input B: ");
-		C = getRealInput("Input C: ");
-
-		getchar(); //Removes existing Enter key from previous inputs
-	}
 	
 	//printf("\nA: %.12lf\nB: %.12lf\nC: %.12lf\n", A, B, C);
 	
@@ -130,7 +71,4 @@ int main(int argc, char **argv) {
 		
 		printf("\n%.12lf +- i%.12lf\n", realSol, imgSol);
 	}
-	
-	close();
-	return 0;
 }
